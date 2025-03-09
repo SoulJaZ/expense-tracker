778ee27b8b5c10a3b511fc94d2c469eea1d57379
RasSoul <mmagtc1101@gmail.com>
2025-03-09 16:41:25 -0500

    creación función que controla la entrada de la información del usuario por la CL

--- expense-tracker.js
+++ expense-tracker.js
@@ -5,3 +5,22 @@ const fs = require("fs");
 const ARCHIVO_TARREAS = path.join(__dirname, "expenses.json");
 
 
+// Función que manjea la entrada de la información del usuario desde la línea de comandos (principal)
+function principal (){
+    mostrarMenu();
+    entradaUsuario.question("Ingrese una opción", (opcion) => {
+        if (entradaUsuario === '1') {
+            entradaUsuario.question("Descripción del gasto: ", (descripcion) => {
+                entradaUsuario.question("Monto: ", (monto) => {
+                    entradaUsuario.question("Fecha: (DD-MM-YY): ", (fecha) => {
+                        agregarGasto(descripcion, monto, fecha);
+                        principal();
+                    })
+                })
+            })
+        }
+        else{
+            
+        }
+    })
+}
