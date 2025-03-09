037ead8c966f0cf4fdc1d859723cd933cff08a1b
RasSoul <mmagtc1101@gmail.com>
2025-03-09 15:27:20 -0500

    intregar expense-tracker con repositorio git

---
+++ expense-tracker.js
@@ -0,0 +1,7 @@
+const path = require("patch");
+const fs = require("fs");
+
+// crear dirección del archivo que almacenara las tareas.
+const ARCHIVO_TARREAS = path.join(__dirname, "expenses.json");
+
+
